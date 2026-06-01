97d9ffcf75319e63517218d59af4ad1a56e258e6
vedantbidwai315-byte <vedantbidwai315@gmail.com>
2026-06-01 14:48:39 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 7;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+    }
+}
